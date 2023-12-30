1bea488efbed68d43e10fe58c2331a865f18abb1
sophavinly18 <https://github.com/lysophavin18/ProjectPratisCumY3>
2023-12-30 22:43:33 +0700

    update Narbar Home page Cor

---
+++ Frontend/darkstorm-pcstore-web/src/components/pages/ProductDeatail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductDeatail = () => {
+  return <div>ProductDeatail</div>;
+};
+
+export default ProductDeatail;
--- Frontend/darkstorm-pcstore-web/src/routers/Routers.js
+++ Frontend/darkstorm-pcstore-web/src/routers/Routers.js
@@ -1,12 +1,18 @@
-import landing from "../pages/Landing";
 import { Routes, Route } from "react-router-dom";
 
 import React from "react";
+import Home from "../components/pages/Home";
+import Login from "../components/pages/Login";
+import { Register } from "../components/pages/Register";
+import ProductDeatail from "../components/pages/ProductDeatail";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path="home" element={landing} />
+      <Route path="home" element={Home} />
+      <Route path="login" element={Login} />
+      <Route path="register" element={Register} />
+      <Route path="productdetail" element={ProductDeatail} />
     </Routes>
   );
 };
