6ff1220211f2e8707cf6157b4079c62d44264c3e
vitou hun <vitouaj68@gmail.com>
2024-01-06 11:51:34 +0700

    add chulay promotion banner

--- Frontend/darkstorm-pcstore-web/src/Pages/LandingPage.jsx
+++ Frontend/darkstorm-pcstore-web/src/Pages/LandingPage.jsx
@@ -2,15 +2,17 @@ import { Card } from "../Components/Card";
 import { PCs as pc } from "../assets/Data/Computer";
 import { Carousel } from "../Components/Carousel";
 import { Layout } from "./Layout";
+import pmt1 from "../assets/Images/PMT.png";
+import pmt2 from "../assets/Images/Promotion.png";
 
 export const LandingPage = () => {
   // handler for card view detail from landing page
 
   // pchum ben carousel
   const img = [
-    "https://i.pinimg.com/originals/d4/3b/15/d43b154119cae525ddbef6bd359e71a4.jpg",
-    "https://img.freepik.com/free-vector/instagram-carousel-templates_23-2148778411.jpg?w=996&t=st=1703347536~exp=1703348136~hmac=cc4ccd7b5d26bfb800bddd3ba51b9cded4664a43940bf6c913f84a18d28e66c1",
-    "https://img.freepik.com/premium-vector/fashion-sale-banner-social-media-instagram-carousel-premium_180271-404.jpg?w=996",
+    // "https://i.pinimg.com/originals/d4/3b/15/d43b154119cae525ddbef6bd359e71a4.jpg",
+    pmt1,
+    pmt2,
   ];
   return (
     <Layout>
